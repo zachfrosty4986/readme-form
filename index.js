d638398c7e623fd70674f45dae12f243a2e696d8
zachfrosty4986 <zachjames0114@gmail.com>
2024-05-05 18:43:35 -0400

    Added files and starter code for challenge

---
+++ index.js
@@ -0,0 +1,13 @@
+// TODO: Include packages needed for this application (defining variables) (inquier package, color) (Packages for node)
+
+// TODO: Create an array of questions for user input (inquirer README docs website)
+const questions = [];
+
+// TODO: Create a function to write README file (Need 'fs')
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app (initialize inquier)
+function init() {}
+
+// Function call to initialize app
+init();
