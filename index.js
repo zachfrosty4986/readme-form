f637761dcc4c011f3b15a85f04cd97360914bcff
zachfrosty4986 <zachjames0114@gmail.com>
2024-05-10 15:28:58 -0400

    Added prompts and liscense functions

--- index.js
+++ index.js
@@ -1,13 +1,81 @@
 // TODO: Include packages needed for this application (defining variables) (inquier package, color) (Packages for node)
-
+const inquirer = require('inquirer');
+const colors = require("colors");
 // TODO: Create an array of questions for user input (inquirer README docs website)
 const questions = [];
+const generateREADME = ({});
 
 // TODO: Create a function to write README file (Need 'fs')
-function writeToFile(fileName, data) {}
+const fs = require("fs");
+function writeToFile(fileName, data) {
+
+  fs.writeFile('README.md', readMePageContent, (err) => {
+    if (err) {
+        console.error(`Error writing to ${fileName}: ${err}`);
+        return;
+    }
+    console.log(`${fileName} has been created successfully.`);
+});
+}
 
 // TODO: Create a function to initialize app (initialize inquier)
-function init() {}
+function init() {
+  inquirer.prompt([
+      {
+        type: 'title',
+        name: 'title',
+        message: 'What is the title of your project?',
+      },
+      {
+        type: 'input',
+        name: 'location',
+        message: 'Where are you from?',
+      },
+      {
+        type: 'input',
+        name: 'hobby',
+        message: 'What is your favorite hobby?',
+      },
+      {
+        type: 'input',
+        name: 'food',
+        message: 'What is your favorite food?',
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username',
+      },
+      {
+        type: 'input',
+        name: 'linkedin',
+        message: 'Enter your LinkedIn URL.',
+      },
+   
+      { 
+        type: "list",
+        message: "What color would you like to use?",
+        name: "color",
+        choices: ["red", "blue", "green", "yellow", "cyan", "magenta", "Black"],
+      },
+      {
+        type: 'confirm',
+        name: 'confirm',
+        message: 'Would you like to submit your Data?',
+      },
+]).then((answers) => {
+  const readMePageContent = generateREADME(answers);
 
+const init = () => {
+  promptUser()
+    // Use writeFile method imported from fs.promises to use promises instead of
+    // a callback function
+    .then((answers) => writeFile('README.md', generateREADME(answers)))
+    .then(() => console.log('Successfully wrote to README.md!'))
+    .catch((err) => console.error(err));
+};
+})
+
+};
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,51 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const licenseCard = 'MIT';
+const licenseBadge = renderLicenseBadge(license);
+function renderLicenseBadge(license) {
+  let licenseBadge = '';
+
+  switch (license) {
+    case 'MIT':
+      licenseBadge = '[]'
+      break;
+    default:
+      licenseBadge = '';
+  }
+  return licenseBadge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {} //if else statements
+const licenseLink = renderLicenseLink(license)
+function renderLicenseLink(license) {
+  let licenseLink = '';
+
+  switch (license) {
+    case 'MIT':
+      licenseLink = '[]'
+      break;
+    default:
+      licenseLink = '';
+  }
+  return licenseLink;
+} //if else statements
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+const licenseSelection = renderLicenseSelection(license)
+function renderLicenseSection(license) { 
+  let licenseSelection = '';
+
+  switch (license) {
+    case 'MIT':
+      licenseSelection = '[]'
+      break;
+    default:
+      licenseSelection = '';
+  }
+  return licenseSelection;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) { //create README template here
