083087f323ae9a787001d23ff8998d6e1559e44f
zachfrosty4986 <zachjames0114@gmail.com>
2024-05-13 17:33:40 -0400

    Adjusted questions, Added addtional license, built README template

--- index.js
+++ index.js
@@ -1,15 +1,63 @@
 // TODO: Include packages needed for this application (defining variables) (inquier package, color) (Packages for node)
 const inquirer = require('inquirer');
+const fs = require("fs");
 const colors = require("colors");
+const generateMarkdown = require("./utils/generateMarkdown")
 // TODO: Create an array of questions for user input (inquirer README docs website)
-const questions = [];
-const generateREADME = ({});
+const questions = [
+  {
+    type: 'title',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Please breifly describe your project',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What are the steps required to install your project?',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Please provide instructions to use your project',
+  },
+  {
+    type: 'input',
+    name: 'license',
+    message: 'Please selct from an MIT, Apache, or Boost license',
+  },
+  {
+    type: 'input',
+    name: 'features',
+    message: 'Please add any additional features your project contains here',
+  },
+  {
+    type: 'input',
+    name: 'collaborators',
+    message: 'Please list all collaborators for this project',
+  },
+  { 
+    type: "list",
+    message: "What color would you like to use?",
+    name: "color",
+    choices: ["red", "blue", "green", "yellow", "cyan", "magenta", "Black"],
+  },
+  {
+    type: 'confirm',
+    name: 'confirm',
+    message: 'Would you like to submit your data now?',
+  },
+];
+
 
 // TODO: Create a function to write README file (Need 'fs')
-const fs = require("fs");
 function writeToFile(fileName, data) {
 
-  fs.writeFile('README.md', readMePageContent, (err) => {
+  fs.writeFile(fileName, data, (err) => {
     if (err) {
         console.error(`Error writing to ${fileName}: ${err}`);
         return;
@@ -20,62 +68,11 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app (initialize inquier)
 function init() {
-  inquirer.prompt([
-      {
-        type: 'title',
-        name: 'title',
-        message: 'What is the title of your project?',
-      },
-      {
-        type: 'input',
-        name: 'location',
-        message: 'Where are you from?',
-      },
-      {
-        type: 'input',
-        name: 'hobby',
-        message: 'What is your favorite hobby?',
-      },
-      {
-        type: 'input',
-        name: 'food',
-        message: 'What is your favorite food?',
-      },
-      {
-        type: 'input',
-        name: 'github',
-        message: 'Enter your GitHub Username',
-      },
-      {
-        type: 'input',
-        name: 'linkedin',
-        message: 'Enter your LinkedIn URL.',
-      },
-   
-      { 
-        type: "list",
-        message: "What color would you like to use?",
-        name: "color",
-        choices: ["red", "blue", "green", "yellow", "cyan", "magenta", "Black"],
-      },
-      {
-        type: 'confirm',
-        name: 'confirm',
-        message: 'Would you like to submit your Data?',
-      },
-]).then((answers) => {
-  const readMePageContent = generateREADME(answers);
-
-const init = () => {
-  promptUser()
-    // Use writeFile method imported from fs.promises to use promises instead of
-    // a callback function
-    .then((answers) => writeFile('README.md', generateREADME(answers)))
-    .then(() => console.log('Successfully wrote to README.md!'))
-    .catch((err) => console.error(err));
+  inquirer.prompt(questions).then((answers) => {
+  writeToFile('New_README.md', generateMarkdown(answers))
+  console.log('Successfully wrote to README.md!')
+  })
 };
-})
 
-};
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,45 +1,54 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-const licenseCard = 'MIT';
-const licenseBadge = renderLicenseBadge(license);
 function renderLicenseBadge(license) {
   let licenseBadge = '';
 
   switch (license) {
     case 'MIT':
-      licenseBadge = '[]'
+      licenseBadge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+      break;
+    case 'Apache': 
+      licenseBadge = `![Static Badge](https://img.shields.io/badge/license-Apache-brightgreen)`
+      break;
+    case 'Boost':
+      licenseBadge = `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`
       break;
     default:
       licenseBadge = '';
+      break;
   }
   return licenseBadge;
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-const licenseLink = renderLicenseLink(license)
 function renderLicenseLink(license) {
   let licenseLink = '';
 
   switch (license) {
     case 'MIT':
-      licenseLink = '[]'
+      licenseLink = `[MIT](https://choosealicense.com/licenses/mit/)`
       break;
+    case 'Apache':
+      licenseLink = `[Apache](http://www.apache.org/licenses/)`
+      break;
+    case 'Boost':
+      licenseLink = `[Boost](https://www.boost.org/users/license.html/)`
     default:
       licenseLink = '';
+      break;
   }
   return licenseLink;
 } //if else statements
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-const licenseSelection = renderLicenseSelection(license)
 function renderLicenseSection(license) { 
   let licenseSelection = '';
 
   switch (license) {
     case 'MIT':
-      licenseSelection = '[]'
+      licenseSelection = `A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.`
       break;
     default:
       licenseSelection = '';
@@ -49,8 +58,44 @@ function renderLicenseSection(license) {
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) { //create README template here
-  return `# ${data.title}
+  return `
+  # ${data.title}
 
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Features](#features)
+  - [Credits](#credits)
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## License
+
+  ${renderLicenseLink(data.license)}
+
+  ${renderLicenseSection(data.license)}
+
+  ## Features
+
+  ${data.features}
+
+  ## Collaborators
+
+  ${data.collaborators}
 `;
 }
 
